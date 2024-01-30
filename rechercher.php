2eafdc15d30a581c1d768328c0a1b77d5899b76a
Tony <127862337+ainzer@users.noreply.github.com>
2024-01-30 16:20:58 +0100

    30/01/2024

--- rechercher.php
+++ rechercher.php
@@ -1,9 +1,9 @@
 <?php
 // Inclure le fichier de connexion à la base de données
-require_once('connexion.php');
+require_once('Model/connexion.php');
 
 // Vérifier si la requête de recherche a été soumise
-if(isset($_GET['query'])) {
+if (isset($_GET['query'])) {
     // Récupérer la requête de recherche depuis l'URL
     $query = $_GET['query'];
 
@@ -12,19 +12,19 @@ if(isset($_GET['query'])) {
         $bdd = dbconnect();
 
         // Préparer la requête SQL pour rechercher dans votre base de données
-        $statement = $bdd->prepare("SELECT * FROM table WHERE colonne LIKE ?");
+        $statement = $bdd->prepare("SELECT * FROM plat WHERE libelle LIKE ?");
         $statement->execute(["%$query%"]);
+
 
         // Récupérer les résultats de la recherche
         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
         // Afficher les résultats (vous pouvez les afficher de la manière qui convient à votre application)
         foreach ($results as $result) {
-            echo "<p>" . $result['colonne'] . "</p>";
+            echo "<p>" . $result['libelle'] . "</p>";
         }
     } catch (PDOException $e) {
         // Gérer l'erreur de connexion de manière appropriée (journalisation, affichage d'un message d'erreur, etc.)
         exit('Erreur de connexion à la base de données : ' . $e->getMessage());
     }
 }
-?>
